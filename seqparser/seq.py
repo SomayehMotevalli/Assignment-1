446969bf7670269d4f5fa834c0177c6c6ec42078
SomayehMotevalli <somayeh.motevalli@gmail.com>
2024-10-23 20:56:02 -0700

    final version

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -16,9 +16,6 @@ def transcribe(seq: str) -> str:
        rna_seq+=(dna_to_rna[base]) # concatenate rna seq after transcribing dna seq
     return rna_seq # return stops the function and gives output after printing
 
-dna_seq='TAGC' # assign dna_seq with a string input
-seq=transcribe(dna_seq) # call the function with dna_seq as an argument
-print(seq)
  
 
 
@@ -27,9 +24,7 @@ def reverse_transcribe(seq: str) -> str: # the function transcribes dna seq to r
     seq=transcribe(seq) # transcribe dna to rna
     reversed_seq=seq[::-1] # reverse rna seq. reverse() method doesn't work on str type input
     return reversed_seq 
-dna='ATCG' # assigned input to dna seq
-reversed_seq=reverse_transcribe(dna) # calling the function
-print(reversed_seq)
+
 
      
 
