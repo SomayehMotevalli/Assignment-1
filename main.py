f48cf969bd8e2589f9c900197ee171650f852de6
SomayehMotevalli <somayeh.motevalli@gmail.com>
2024-10-23 20:32:19 -0700

    updated the codes

--- main.py
+++ main.py
@@ -11,46 +11,35 @@ def main():
     # Create instance of FastaParser
     aparser = FastaParser('data/test.fa') # aparser is an instance
     # Create instance of FastqParser
-        
+    qparser = FastqParser('data/test.fq')
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
-    parser_obj = FastaParser('data/test.fa')
-    for header,sequence in parser_obj:
-        print(header)
-        print(transcribe(sequence))
-    parser_obj = FastaParser('data/test.fa')
-    for record in parser_obj:
-        header,sequence=record
-        seq=sequence
-        seq=transcribe(seq)
-        reversed_seq=seq[::-1]
+    for header,sequence in aparser:
+        seq= transcribe(sequence)
+        print(seq)
+     # For each record of FastaParser, Reverse Transcribe the sequence
+    # and print it to console
+       
+    for header, sequence in aparser:
+        reversed_seq= reverse_transcribe(sequence)
         print(reversed_seq)
        
         
-   
 
-
-
-        
-            
-    aparser = FastqParser('data/test.fq')
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
-    
-    parser_obj = FastqParser('data/test.fq')
-    for record in parser_obj:
-        header,sequence,quality=record
+
+    for header, sequence, quality in qparser:
         seq=transcribe(sequence)
-        reversed_seq=seq[::-1]
-        print(reversed_seq)
-     
+        print(seq)
      
 
-    # For each record of FastaParser, Reverse Transcribe the sequence
-    # and print it to console
-       
+   
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
+    for header, sequence, quality in qparser:
+        reversed_seq= reverse_transcribe(sequence)
+        print(reversed_seq)
 
 
 """
